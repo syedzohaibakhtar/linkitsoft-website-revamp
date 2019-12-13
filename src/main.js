258ca89715abf0ea6ecfa3c6940d06d9fdbccc88
Zohaib <syedzohaib.akhtar@myeg.com.my>
2019-12-14 05:06:40 +0800

    dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
+
+require('./assets/sass/main.scss');
 
 Vue.config.productionTip = false
 
